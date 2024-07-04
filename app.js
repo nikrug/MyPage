8b44bfb21ae2f43a0ac716743aae28742a14186a
Никита Домрачев <domrachev-nikitka@mail.ru>
2024-07-05 00:49:23 +0700

    был добавлен анимированый фон gif и картинки новые

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ VANTA.BIRDS({
   touchControls: true,
   gyroControls: false,
   minHeight: 1200.00,
-  minWidth: 50.00,
+  minWidth: 100.00,
   scale: 1.00,
   scaleMobile: 1.00,
   backgroundColor: 0xffffff,
