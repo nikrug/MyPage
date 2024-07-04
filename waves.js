79cdbd2cc9389f51dd74072527c50abd384b40cb
Никита Домрачев <domrachev-nikitka@mail.ru>
2024-07-05 00:39:35 +0700

    был добавлен анимированый фон gif и картинки новые

--- waves.js
+++ waves.js
@@ -2,9 +2,9 @@ VANTA.WAVES({
   el: "#intro",
   mouseControls: true,
   touchControls: true,
-  gyroControls: true,
+  gyroControls: false,
   minHeight: 1190.00,
-  minWidth: 200.00,
+  minWidth: 50.00,
   scale: 1.00,
   scaleMobile: 1.00
 })
