a2262608f13e4dfcec8f50a92af3d59362866ac3
Никита Домрачев <domrachev-nikitka@mail.ru>
2024-07-05 02:23:17 +0700

    починены анимированый фон когда он не мог воспроизводиться при скроле

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
 
 VANTA.BIRDS({
+
   el: "#intro",
+                 el: "#intro",
                 mouseControls: true,
                 touchControls: true,
                 gyroControls: true,
                 forceAnimate: true,
-                minHeight: 1920.00,
+                minHeight: 1900.00,
                 minWidth: 1080.00,
                 scale: 1.00,
                 scaleMobile: 1.00,
                 color1: 0xde0707,
                 color2: 0x7c0eb,
                 birdSize: 0.60,
-                separation: 14.00,
+                separation: 114.00,
                 alignment: 26.00,
                 backgroundAlpha: 0
+
 })
